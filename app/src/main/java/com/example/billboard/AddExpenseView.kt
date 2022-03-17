08fc74504fadf7f729688bf57973e6559e701d65
Clémence <clemence.cunin@gmail.com>
2022-03-17 15:53:44 +0200

    Expense view added and some changes in AddView

--- app/src/main/java/com/example/billboard/AddExpenseView.kt
+++ app/src/main/java/com/example/billboard/AddExpenseView.kt
@@ -1,5 +1,6 @@
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowDropDown
@@ -8,18 +9,25 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.onSizeChanged
 import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.input.KeyboardType
+import com.example.billboard.R
 
 
 @Composable
-fun AddExpenseView() {
+fun AddExpenseView(id : String) {
 
     var menuExpanded by remember { mutableStateOf(false) }
 
     //TODO Has to be changed by fetching the group members
-    var groupMembers: List<String> = mutableListOf("John", "Maeve")
+    /*
+    if the id is not empty -> fetch the data to prefill the edit formgit
+     */
+    val groupMembers: List<String> = mutableListOf("John", "Maeve", "Davis", "Emma", "Carl")
 
     var payerMember: String by remember { mutableStateOf("") }
-    var membersWhoPay : MutableList<String> = mutableListOf("")
+
+    val membersWhoPay : MutableList<String> = mutableListOf("")
 
     var dropDownWidth by remember { mutableStateOf(0) }
 
@@ -33,9 +41,10 @@ fun AddExpenseView() {
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Text(text = "BillBoard")
+        Text(text = "Add a new expense line")
         OutlinedTextField(value = expenseName, onValueChange = {expenseName = it}, label = { Text(text = "Expense name") })
         //TODO only allowed numbers in this textfield -> function
-        OutlinedTextField(value = expenseAmount, onValueChange = {expenseAmount = it}, label = { Text(text = "Expense amount") })
+        OutlinedTextField(value = expenseAmount, onValueChange = {expenseAmount = it}, label = { Text(text = "Expense amount") }, keyboardOptions = KeyboardOptions.Default.copy(keyboardType = KeyboardType.Number))
         Column() {
             OutlinedTextField(
                 value = payerMember,
@@ -59,6 +68,7 @@ fun AddExpenseView() {
                 groupMembers.forEach { member ->
                     DropdownMenuItem(onClick = {
                         payerMember = member
+                        if(membersWhoPay.contains(payerMember)) membersWhoPay.remove(payerMember)
                     }) {
                         Text(text = member)
                     }
@@ -75,19 +85,48 @@ fun AddExpenseView() {
                 }
             }
         }
-        //TODO function which add a new expense in the database
-        Button(onClick = {}){
+        //TODO function which add a new expense in the database and updated the balance row in group collection
+        var expenseInfo: MutableState<String> = remember { mutableStateOf("")}
+        Button(onClick = {
+            if(expenseName.isNotEmpty() && expenseAmount.isNotEmpty() && payerMember.isNotEmpty() && membersWhoPay.isNotEmpty())
+                showExpenseData(
+                    name = expenseName,
+                    amount = expenseAmount,
+                    payer = payerMember,
+                    membersWhoPay = membersWhoPay,
+                    expenseInfo = expenseInfo)
+            }){
             Text(text = "Add a new expense line")
+        }
+        if(expenseInfo.value.isNotEmpty()){
+            Text(text = expenseInfo.value)
+        }
+
+        Row(
+            horizontalArrangement = Arrangement.Start
+        ){
+            Icon(
+                painter = painterResource(id = R.drawable.ic_back),
+                contentDescription = "back icon",
+                modifier = Modifier.clickable {  /*TODO add navigation*/  })
         }
     }
 }
 
 @Composable
 fun CheckBox(member : String, membersWhoPay : MutableList<String>){
-    val checkState = remember {mutableStateOf(true)}
+    val checkState = remember {mutableStateOf(false)}
     Checkbox(
         checked = checkState.value,
-        onCheckedChange = { checkState.value = it; if(checkState.value) membersWhoPay.add(member) else membersWhoPay.remove(member)   }
+        onCheckedChange = { checkState.value = it; if(checkState.value) membersWhoPay.add(member) else membersWhoPay.remove(member)  }
     )
 }
 
+//TODO when updating with add to database function, the following function has to be deleted
+fun showExpenseData(name : String, amount : String, payer : String, membersWhoPay : MutableList<String>, expenseInfo : MutableState<String>){
+    expenseInfo.value = "Expense line data : Name -> $name, Amount -> $amount, Payer -> $payer, Members who pay -> "
+    membersWhoPay.forEach { member ->
+        expenseInfo.value = expenseInfo.value + member + " "
+    }
+
+}
